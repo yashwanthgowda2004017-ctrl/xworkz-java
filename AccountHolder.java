4522fa6832b7dfb15359f7e79c03bc7f622abbe6
yashwanthgowda2004017-ctrl <yashwanthgowda2004017@gmail.com>
2026-02-05 13:58:18 +0530

    initial file

---
+++ AccountHolder.java
@@ -0,0 +1,18 @@
+class AccountHolder{
+	
+	public static void main(String[] AccountHolder){
+		String accountholderName = "Yashwanth";
+		long  accountNumber = 0312 01000L;
+		String  ifscCode = "IOBA0000312";
+		long mobileNumber = 7019512166L;
+		Double bankBalance = 100.15;
+		
+		System.out.println("The AccountHolder Name is:"+accountholderName);
+		System.out.println("Account number:"+accountNumber);
+		System.out.println("ifscCode:"+ifscCode);
+		System.out.println("mobile number :"+mobileNumber);
+		System.out.println("bank balance is:"+bankBalance);
+	}
+}
+		
+	
